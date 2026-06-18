a5626de3203881e1f1f21c94a4fcdbc038037520
MidHunterX <midhunterx@gmail.com>
2026-06-18 16:03:39 +0530

    ♻️ refactor: move app definitions to app.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:shopping_assist/main.dart';
+import 'package:shopping_assist/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
