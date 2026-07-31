1bb089e59b930ffec09c1f77d899d35b0c163d52
MidHunterX <midhunterx@gmail.com>
2026-08-01 01:59:20 +0530

    💄 feat(ui): when possible show grams/oz in purchased_item_tile

--- lib/features/purchased_items/views/widgets/purchased_item_tile.dart
+++ lib/features/purchased_items/views/widgets/purchased_item_tile.dart
@@ -221,7 +221,11 @@ class PurchasedItemTile extends StatelessWidget {
             fit: BoxFit.scaleDown,
             child: Text(
               pItem.isWeight
-                  ? pItem.quantity!.toQuantityString(context.weightUnit)
+                  ? pItem.quantity! < 1.0
+                        ? (pItem.quantity! * 1000).toQuantityString(
+                            context.weightUnit == 'kg' ? 'g' : 'oz',
+                          )
+                        : pItem.quantity!.toQuantityString(context.weightUnit)
                   : pItem.quantity! >= 1.0
                   ? pItem.quantity!.toQuantityString('')
                   : pItem.quantity! == 0.25
